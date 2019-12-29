621db221cad6b63808a7a4d9f43909f6443e0759
MaJiGerman <pedroa786@gmail.com>
2019-12-29 18:48:39 +0100

    movimiento fluido

--- tuto - copia/Assets/Scripts/PlayerMovement.cs
+++ tuto - copia/Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = transform.right * speed;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = -transform.right * speed;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.velocity = new Vector3(0, 0, 1) * speed;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.velocity = new Vector3(0, 0, -1) * speed;
-        }
+        transform.Translate(speed*Input.GetAxis("Horizontal")*Time.deltaTime,0.0f,speed*Input.GetAxis("Vertical")*Time.deltaTime);
     }
 }
