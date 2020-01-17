af4344573b3f1782b6b388d321c789e2f51d31cc
MaJiGerman <pedroa786@gmail.com>
2020-01-17 20:25:17 +0100

    corutines añadidas

--- tuto - copia/Assets/Scripts/Jugador.cs
+++ tuto - copia/Assets/Scripts/Jugador.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour 
 {
-
-	
 	// Variables Globales
 	private DB_Script db;
 	public string CodSer;
@@ -22,7 +20,7 @@ public class Jugador : MonoBehaviour
 		int randomNumber = random.Next(0, 2048);
 		CodSer = "UNITY_"+randomNumber;
 		activo = false;
-		HP = 10;
+		HP = 3;
 		size = 0.05f;
 		//Debug.Log("CODSER: "+CodSer+" esta ahora activo");
 
@@ -74,31 +72,28 @@ public class Jugador : MonoBehaviour
 			activo = false;
 			Debug.Log("CODSER: "+CodSer+" going DARK");
         }
-		//COMPROBAR VIDA
-		if (HP<=0)
-		{
-			//Debug.Log("AUTODESTROYENDOSE");
-			db.BorrarUsuario(CodSer);
-			Destroy(gameObject);
-		}
+
 	}
 	void OnCollisionEnter(Collision col)
     {
-		if(this.gameObject.layer == 10 && col.gameObject.layer == 9
-			|| this.gameObject.layer == 9 && col.gameObject.layer == 11 
-			|| this.gameObject.layer == 11 && col.gameObject.layer == 10)
-		{
-			this.transform.localScale += new Vector3(-size, -size, -size);
-			db.BorrarListaUsuario(CodSer,"BORRADO");
-			HP = db.ObtenerLongitudLista(CodSer);
-		}
-		if(this.gameObject.layer == 9 && col.gameObject.layer == 10
-			|| this.gameObject.layer == 11 && col.gameObject.layer == 9 
-			|| this.gameObject.layer == 10 && col.gameObject.layer == 11)
-		{
-			this.transform.localScale += new Vector3(size, size, size);
-			db.AniadirListaUsuario(CodSer,"ANIADIDO");
-			HP = db.ObtenerLongitudLista(CodSer);
+		if(true){ //si existe la instancia
+			if(this.gameObject.layer == 10 && col.gameObject.layer == 9
+				|| this.gameObject.layer == 9 && col.gameObject.layer == 11 
+				|| this.gameObject.layer == 11 && col.gameObject.layer == 10)
+			{
+				if(HP>5)
+				{
+					StartCoroutine(escalarSuave(1,-size));
+				}	
+				StartCoroutine(borrarDato(this.CodSer));
+			}
+			if(this.gameObject.layer == 9 && col.gameObject.layer == 10
+				|| this.gameObject.layer == 11 && col.gameObject.layer == 9 
+				|| this.gameObject.layer == 10 && col.gameObject.layer == 11)
+			{
+				StartCoroutine(escalarSuave(1,size));
+				StartCoroutine(aniadirDato(this.CodSer));
+			}
 		}
     }
 	public void activarJugador(bool activar)
@@ -107,5 +102,45 @@ public class Jugador : MonoBehaviour
 			this.activo=true;
 		else
 			this.activo=false;
+	}
+
+	private IEnumerator aniadirDato(string CodSer) 
+	{
+		db.AniadirListaUsuario(CodSer,"ANIADIDO");
+		StartCoroutine(leerLongitud(CodSer));
+		yield return null;
+	}
+
+	private IEnumerator borrarDato(string CodSer) 
+	{
+		db.BorrarListaUsuario(CodSer,"BORRADO");
+		StartCoroutine(leerLongitud(CodSer));
+		yield return null;
+	}
+	private IEnumerator escalarSuave(float time, float scale_size)
+    {
+        Vector3 originalScale = this.transform.localScale;
+        Vector3 destinationScale = this.transform.localScale + new Vector3(scale_size, scale_size, scale_size);
+         
+        float currentTime = 0.0f;
+         
+        while (currentTime <= time)
+        {
+            this.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        };
+	}
+	private IEnumerator leerLongitud(string CodSer) 
+	{
+		HP = db.ObtenerLongitudLista(CodSer);
+		//COMPROBAR VIDA
+		if (HP<=0)
+		{
+			//Debug.Log("AUTODESTROYENDOSE");
+			db.BorrarUsuario(CodSer);
+			Destroy(gameObject);
+		}
+		yield return null;
 	}
 }
